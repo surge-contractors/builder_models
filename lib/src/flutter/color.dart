10611a7a80524c653971e243ad9de617874447db
Stephen (Alex) Wallen <wallenstephen@outlook.com>
2024-02-28 14:36:01 -1000

    chore: add luminance extension to color

--- lib/src/flutter/color.dart
+++ lib/src/flutter/color.dart
@@ -9,3 +9,12 @@ class Color with _$Color {
   static const Color white = Color(0xFFFFFFFF);
   static const Color black = Color(0xFF000000);
 }
+
+extension Luminance on Color {
+  double get luminance {
+    final r = ((value >> 16) & 0xFF) / 255;
+    final g = ((value >> 8) & 0xFF) / 255;
+    final b = (value & 0xFF) / 255;
+    return 0.2126 * r + 0.7152 * g + 0.0722 * b;
+  }
+}
